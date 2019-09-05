b9e2e5c50fa0c926aba746205fef3c8917c5e724
Wang <ideaqiwang@gmail.com>
2019-09-04 23:47:14 -0400

    add comments

--- 310_MinimumHeightTrees.py
+++ 310_MinimumHeightTrees.py
@@ -11,12 +11,19 @@ class Solution:
     for p, q in edges:
       adj[p].add(q)
       adj[q].add(p)
-    
+    '''
+      A leaf is a node only connects one other node.
+      Find all leaf nodes, then trim them until the tree has 2 nodes at most.
+    '''
     leaves = [i for i in range(n) if len(adj(i)) == 1]
     
     while n > 2:
       n -= len(leaves);
       newLeaves = []
+      '''
+        Find the connected node to the leaf node, and remove the leaf.
+        And update new leaf nodes.
+      '''
       for leaf in leaves:
         otherNode = adj[leaf].pop()
         adj[otherNode].remove(leaf)
