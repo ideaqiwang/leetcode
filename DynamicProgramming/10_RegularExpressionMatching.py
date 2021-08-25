dfab851e602458b18526e5898b65df887c0fd0c7
Wang Qi <ideaqiwang@gmail.com>
2021-08-25 22:58:00 +0100

    update

--- DynamicProgramming/10_RegularExpressionMatching.py
+++ DynamicProgramming/10_RegularExpressionMatching.py
@@ -43,15 +43,18 @@ class Solution:
         dp[0][0] = True
         
         for j in range(1, n+1):
-            if p[j-1] == "*":
+            if p[j-1] == "*": # count 'a*' as empty
                 dp[0][j] = dp[0][j-2]
         
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if p[j-1] == "*":
-                    dp[i][j] = dp[i][j-2]
+                    dp[i][j] = dp[i][j-2] # 'a*' counts as empty
                     if s[i-1] == p[j-2] or p[j-2] == ".":
-                        dp[i][j] = dp[i][j] or dp[i-1][j] 
+                        # dp[i][j-1] => 'a*' counts as single a
+                        # dp[i-1][j] => 'a* counts as multile a
+                        # dp[i][j-2] => 'a*' counts as empty
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
                 else:
                     if s[i-1] == p[j-1] or p[j-1] == ".":
                         dp[i][j] = dp[i-1][j-1]
